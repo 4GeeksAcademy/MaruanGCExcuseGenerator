2d63b19d49feefb9fe9eec1835c65f35bd46355b
Maruan Yarbouh <146507031+maruangc@users.noreply.github.com>
2023-11-08 03:04:05 +0000

    excuses exercise finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 window.onload = function() {
-  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  document.getElementById("excuse").innerHTML =
+    who[random(0, 3)] +
+    " " +
+    action[random(0, 3)] +
+    " " +
+    what[random(0, 2)] +
+    " " +
+    when[random(0, 4)];
+
   console.log("Hello Rigo from the console!");
 };
